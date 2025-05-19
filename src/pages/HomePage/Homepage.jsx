8283e78d3f63a4c900372ec2eac09a460ae8e127
yungflashh <streaktv8@gmail.com>
2025-05-19 23:06:44 +0100

    tested the tailwind

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Homepage = () => {
   return (
-    <div>WELCOME TO THE LANDING PAGE</div>
+    <div className='text-2xl text-center'>WELCOME TO THE LANDING PAGE</div>
   )
 }
 
