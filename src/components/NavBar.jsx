d96513f2f17e678758c77d151986f59e624fe958
yungflashh <streaktv8@gmail.com>
2025-05-21 08:53:13 +0100

    adding the footer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,33 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { RxHamburgerMenu } from "react-icons/rx";
-import sail_Logo from "../assets/images/svg/sail_Logo.svg"
-
+import sail_Logo from "../assets/images/svg/sail_Logo.svg";
+import Sidebar from './SideBar'; 
 const NavBar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
-    <div className=' flex justify-around items-center bg-white pt-2.5 pb-2.5'>
-        <div className="logo-container ">
-            <img src={sail_Logo} alt="Company Logo" />
-
+    <>
+      <div className="flex justify-between items-center px-4 py-2 bg-white/50 backdrop-blur-md h-20 sm:px-6 md:px-10 w-full fixed top-0 z-40">
+        
+        {/* Logo */}
+        <div className="logo-container flex-shrink-0">
+          <img src={sail_Logo} alt="Company Logo" className="h-8 sm:h-10" />
         </div>
 
-        <div className=''>
-            <RxHamburgerMenu/>
+        {/* Hamburger Icon */}
+        <div
+          className="sm:hidden text-2xl text-[#627A95] cursor-pointer"
+          onClick={() => setIsSidebarOpen(true)}
+        >
+          <RxHamburgerMenu />
         </div>
 
-
-        <div className="links flex gap-8 text-[#627A95]">
-           <p className='sora text-sm hover:text-[#343639] hover:'>Home</p>
-           <p className='sora text-sm hover:text-[#133C8A]'>Courses</p>
-           <p className='sora text-sm hover:text-[#133C8A]'>Testimonials</p>
-           <p className='sora text-sm hover:text-[#133C8A]'>FAQs</p>
+        {/* Desktop Nav */}
+        <div className="hidden sm:flex gap-6 text-[#627A95] text-sm">
+          <p className="sora hover:text-[#343639] cursor-pointer">Home</p>
+          <p className="sora hover:text-[#133C8A] cursor-pointer">Courses</p>
+          <p className="sora hover:text-[#133C8A] cursor-pointer">Testimonials</p>
+          <p className="sora hover:text-[#133C8A] cursor-pointer">FAQs</p>
         </div>
 
-
-        <div className=''>
-            <input type="" placeholder='Search' className='w-42 h-10 pl-4 text-sm border-[1px] mr-2 border-[#627A95] rounded-[8px]' />
+        {/* Search Input */}
+        <div className="hidden sm:block">
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-40 h-9 pl-3 text-sm border border-[#627A95] rounded-md"
+          />
         </div>
-    </div>
-  )
-}
+      </div>
 
-export default NavBar
+      {/* Sidebar */}
+      <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+    </>
+  );
+};
+
+export default NavBar;
