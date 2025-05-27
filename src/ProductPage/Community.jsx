9bd0e1549cc9f11645ad4d0db1fc113deb41a600
0xidennis <meneweifeanyi5@gmail.com>
2025-05-27 14:37:24 -0700

    first commit

--- src/ProductPage/Community.jsx
+++ src/ProductPage/Community.jsx
@@ -5,11 +5,11 @@ const Community = () => {
     <div>
          <div className="bg-[#133C8A] text-white p-15 max-w-8xl">
       <div className="mb-2">
-        <span className="bg-yellow-500 text-xs font-semibold px-2 py-1 rounded text-black">How to apply</span>
+        <span className="bg-yellow-500 text-xs font-semibold px-2 py-1  text-white rounded-2xl">How to apply</span>
       </div>
-      <h2 className="text-2xl font-bold mb-3">Ready to Join Our Community?</h2>
+      <h2 className="text-2xl font-bold mb-3">Ready to Join <br /> Our Community?</h2>
       <p className="text-sm mb-6 text-blue-100">
-      Take the first step towards an exciting career in Product Design & UI/UX!. Click the Apply Now button below and fill out the application form. Our team will review your application and reach out if you qualify.
+      Take the first step towards an exciting career in Product Design & UI/UX!. <br />Click the Apply Now button below and fill out the application form. <br /> Our team will review your application and reach out if you qualify.
       </p>
       <button className="bg-white text-blue-800 px-4 py-2 rounded-md text-sm font-medium hover:bg-blue-50 transition-colors">
         APPLY NOW
