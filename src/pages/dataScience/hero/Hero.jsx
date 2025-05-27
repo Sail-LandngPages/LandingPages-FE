aa0326b1c85b99486662088f3f012d2fdd37e2e9
okechukwu samuel okolie <okechukwu.okolie.2023@gmail.com>
2025-05-27 07:44:27 +0100

    added animation to the pages

--- src/pages/dataScience/hero/Hero.jsx
+++ src/pages/dataScience/hero/Hero.jsx
@@ -13,21 +13,29 @@ const Hero = () => {
             className="hero-title"
             initial={{opacity:0, scale: 0.1 }}
             animate={{opacity:1, scale: 1 }}
-            transition={{ type: "spring", stiffness: 400, damping: 10 , duration:3}}>
+            transition={{ type: "spring", stiffness: 400, damping: 10 , duration:5}}>
               <span>Data</span> is the new gold! <br /> it's time to dig</motion.h1>
             <motion.p 
             className="hero-paragraph"
               initial={{ x: -100,  }}
-              animate={{ x:0,x:100,x:0}}
-              transition={{ type: "spring", stiffness: 400, damping: 10 , duration:3}}>
+              animate={{ x:0 }}
+              transition={{ type: "spring", stiffness: 400, damping: 10 , duration:5}}>
                 Are you ready to turn raw numbers into powerful insights? 
             The SAIL Data Science learning Track is your ticket to mastering data, analytics and AI whie working on real-world projects that make  you job-ready</motion.p>
             
             <div className="hero-button">
-              <button className="join-now">Join Now</button>
+              <motion.button
+               className="join-now"
+               initial={{ y: 100,  }}
+               animate={{ y:0 }}
+               transition={{duration:5, type: "spring", stiffness: 400, damping: 10 }}>
+                Join Now</motion.button>
             </div>
         </div>
-            <img src={hero_image} alt="" />
+            <motion.img 
+             initial={{ opacity: 0, scale: 0.1 }}
+             animate={{ opacity: 1, scale: 1 }}
+             transition={{duration:1}} src={hero_image} alt="" />
         </div>
   )
 }
