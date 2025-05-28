66d492fbd73a5f2ae880b9bfd72c29e9243bc176
0xidennis <meneweifeanyi5@gmail.com>
2025-05-28 04:25:19 -0700

    first commit

--- src/ProductPage/Program.jsx
+++ src/ProductPage/Program.jsx
@@ -49,9 +49,20 @@ const Program = () => {
       <header className="mb-8 space-y-4 lg:ml-10">
          
 
-        <button className='bg-[#FBB404]'>
-          
-          <span className="inline-block  text-[#ffff] text-xs font-medium px-3 py-1 rounded-full">
+        <button className='bg-[#FBB404] items-center text-white justify-center rounded-full flex hover:bg-[#133C8A]'>
+           <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 -mr-2"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+          <span className="inline-block  text-[#ffff] text-xs font-medium px-3 py-1">
           
           Benefits
         </span>
