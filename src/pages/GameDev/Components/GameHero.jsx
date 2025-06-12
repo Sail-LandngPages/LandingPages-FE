9bdbf75c7b1d093b1e3915d31009b08e097e4827
Chumexxx <obasyemeka@gmail.com>
2025-06-12 04:42:29 +0100

    cgukwuemeks/gameDev

--- src/pages/GameDev/Components/GameHero.jsx
+++ src/pages/GameDev/Components/GameHero.jsx
@@ -1,39 +1,540 @@
-import backgroundimage from "./../../../assets/gamedev-assets/gamedevBackgroundImage.svg"
-import gamer from "./../../../assets/gamedev-assets/gamer.svg"
-import star from "./../../../assets/gamedev-assets/star.svg"
+import styled from "styled-components";
+import backgroundimage from "./../../../assets/gamedev-assets/gamedevBackgroundImage.svg";
+import gamer from "./../../../assets/gamedev-assets/gamer.svg";
+import star from "./../../../assets/gamedev-assets/star.svg";
 import "@fontsource/orbitron";
 
-const GameHero = () => {
-    return(
-        <div className="w-full p-[130px] bg-[#111827] bg-cover bg-center relative flex justify-center items-center" style={{ backgroundImage: `url(${backgroundimage})` }}>
-            <div className="w-[1400px] h-[600px] flex  items-center justify-around">
-                <div>
-                    <h1 className="font-[orbitron] font-bold text-[18px] text-[#FBAD04] mb-[20px]">LEVEL UP YOUR SKILLS</h1>
-                    <h3 className="font-[orbitron] font-bold text-[40px] text-white mb-[60px]">THINK YOU CAN OUTBUILD <br /> <span className="font-orbitron font-bold text-[40px] text-[#FBAD04]">COD?</span></h3>
-                    <div className="w-[600px] h-[150px] border-l-[4px] border-[#61B8A8] pl-[20px] mb-[40px] flex justify-center items-center">
-                        <p className="text-gray-400 text-[20px] font-[450]">Want to design your own video games, bring characters to life, and create experiences that millions can play? The SAIL Game Development Learning Track is your shortcut to becoming a game dev pro!</p>
-                    </div>
-                    <div className="flex justify-center items-center gap-7">
-                        <button className="w-[250px] h-[45px] font-[orbitron] font-bold text-black text-[15px] bg-[#FBAD04] rounded-lg shadow-[bottom] cursor-pointer hover:bg-[#61B8A8] transition-all duration-300 border-b-4 border-[#926b18]">START YOUR QUEST HERE</button>
-                        <button className="w-[250px] h-[45px] font-medium text-white text-[15px] font-[orbitron] border-[#61B8A8] border-[3px] rounded-lg cursor-pointer hover:border-[#61B8A8] transition-all duration-300">VIEW SKILL TREE</button>
-                    </div>
-                </div>
-                <div className="relative">
-                    <img src={gamer} alt="Gamer" className="w-[700px] h-[650px]" />
-                    <div className="border-[2px] border-[#FBAD04] p-[10px] rounded-lg w-[250px] bg-[#111827] gap-2] absolute top-[380px] left-[450px]">
-                        <div className="flex gap-2">
-                            <img src={star} alt="Star" className="" />
-                            <p className="font-[orbitron] font-medium text-[#FBAD04] text-[20px]">100% <br />SCHOLARSHIPS</p>
-                        </div>
 
-                        <div>
-                            <p className="text-[white]">Limited slots available! Apply now to secure your spot in the next cohort</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+const GameHero = () => {
+  return (
+    <HeroContainer>
+      <InnerContainer>
+        <TextSection>
+          <Heading1>LEVEL UP YOUR SKILLS</Heading1>
+          <Heading3>
+            THINK YOU CAN OUTBUILD <br />
+            <span>COD?</span>
+          </Heading3>
+          <DescriptionContainer>
+            <Description>
+              Want to design your own video games, bring characters to life, and create experiences that millions can play? The SAIL Game Development Learning Track is your shortcut to becoming a game dev pro!
+            </Description>
+          </DescriptionContainer>
+          <ButtonContainer>
+            <PrimaryButton>START YOUR QUEST</PrimaryButton>
+            <SecondaryButton>VIEW SKILL TREE</SecondaryButton>
+          </ButtonContainer>
+        </TextSection>
+        <ImageSection>
+          <GamerImage src={gamer} alt="Gamer" />
+          <ScholarshipBox>
+            <ScholarshipFlex>
+              <img src={star} alt="Star" style={{ width: '24px', height: '24px' }} />
+              <ScholarshipText>
+                100% <br />
+                SCHOLARSHIPS
+              </ScholarshipText>
+            </ScholarshipFlex>
+            <ScholarshipDescription>
+              Limited slots available! Apply now to secure your spot in the next cohort
+            </ScholarshipDescription>
+          </ScholarshipBox>
+        </ImageSection>
+      </InnerContainer>
+    </HeroContainer>
+  );
+};
 
 export default GameHero;
+
+const HeroContainer = styled.div`
+  width: 100%;
+  padding: 130px;
+  background-color: #111827;
+  background-image: url(${backgroundimage});
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 1440px) {
+    padding: 50px;
+  }
+  @media (max-width: 1024px) {
+    padding: 0px;
+  }
+  @media (max-width: 768px) {
+    padding-top: 400px;
+    padding-bottom: 400px;
+  }
+  @media (max-width: 425px) {
+    padding: 14px;
+  }
+  @media (max-width: 390px) {
+    padding: 14px;
+  }
+  @media (max-width: 375px) {
+    padding: 16px; 
+  }
+`;
+
+const InnerContainer = styled.div`
+  width: 1400px;
+  height: 600px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  @media (max-width: 1440px) {
+    width: 90%;
+    height: 500px;
+  }
+  @media (max-width: 1024px) {
+    width: 95%;
+    height: 450px;
+  }
+  @media (max-width: 768px) {
+    height: auto; 
+  }
+  @media (max-width:500px) {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    gap: 24px;
+  }
+  @media (max-width:425px) {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    gap: 24px;
+  }
+  @media (max-width: 390px) {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    gap: 24px;
+  }
+  @media (max-width: 375px) {
+    gap: 16px;
+  }
+`;
+
+const TextSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (min-width: 768px) {
+    align-items: flex-start;
+  }
+`;
+
+const Heading1 = styled.h1`
+  font-family: "Orbitron", sans-serif;
+  font-weight: 700;
+  font-size: 18px;
+  color: #fbad04;
+  margin-bottom: 20px;
+
+  @media (max-width: 1440px) {
+    font-size: 16px; 
+  }
+  @media (max-width: 1024px) {
+    font-size: 14px;
+  }
+  @media (max-width: 768px) {
+    font-size: 10px; 
+  }
+  @media (max-width: 425px) {
+    font-size: 10px;
+    text-align: left;
+  }
+  @media (max-width: 390px) {
+    font-size: 10px;
+    text-align: left;
+  }
+  @media (max-width: 375px) {
+    font-size: 10px;
+  }
+`;
+
+const Heading3 = styled.h3`
+  font-family: "Orbitron", sans-serif;
+  font-weight: 700;
+  font-size: 40px;
+  color: white;
+  margin-bottom: 60px;
+  text-align: center;
+
+  & span {
+    color: #fbad04;
+  }
+
+  @media (min-width: 768px) {
+    text-align: left;
+  }
+  @media (max-width: 1440px) {
+    font-size: 36px;
+    margin-bottom: 48px;
+  }
+  @media (max-width: 1024px) {
+    font-size: 20px; 
+    margin-bottom: 20px;
+  }
+  @media (max-width: 768px) {
+    font-size: 13px; 
+    margin-bottom: 12px;
+  }
+  @media (max-width: 425px) {
+    font-size: 24px; 
+    text-align: left;
+  }
+  @media (max-width: 390px) {
+    font-size: 24px; 
+    text-align: left;
+  }
+  @media (max-width: 375px) {
+    font-size: 20px; 
+  }
+`;
+
+const DescriptionContainer = styled.div`
+  width: 600px;
+  height: 150px;
+  border-left: 4px solid #61b8a8;
+  padding-left: 20px;
+  margin-bottom: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 1440px) {
+    width: 500px;
+    height: 130px;
+  }
+  @media (max-width: 1024px) {
+    height: 120px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+    height: auto;
+    padding: 10px;
+    margin-bottom: 14px;
+  }
+  @media (max-width: 425px) {
+    padding: 12px;
+  }
+  @media (max-width: 390px) {
+    padding: 12px;
+  }
+  @media (max-width: 375px) {
+    padding: 10px; 
+  }
+`;
+
+const Description = styled.p`
+  color: #9ca3af;
+  font-size: 20px;
+  font-weight: 450;
+  text-align: left;
+
+  @media (max-width: 1440px) {
+    font-size: 18px;
+  }
+  @media (max-width: 1024px) {
+    font-size: 16px; 
+  }
+  @media (max-width: 768px) {
+    font-size: 10px;
+    text-align: left;
+  }
+    @media (max-width: 425px) {
+    font-size: 12px; 
+    text-align: left;
+  }
+  @media (max-width: 390px) {
+    font-size: 12px; 
+    text-align: left;
+  }
+  @media (max-width: 375px) {
+    font-size: 11px; 
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 28px;
+
+  @media (max-width: 768px) {
+    gap: 16px;
+  }
+  @media (max-width: 425px) {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 12px;
+  }
+  @media (max-width: 390px) {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 12px;
+  }
+  @media (max-width: 375px) {
+    gap: 10px; 
+  }
+`;
+
+const PrimaryButton = styled.button`
+  width: 250px;
+  height: 45px;
+  font-family: "Orbitron", sans-serif;
+  font-weight: 700;
+  font-size: 15px;
+  color: black;
+  background-color: #fbad04;
+  border-radius: 8px;
+  border-bottom: 4px solid #926b18;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:hover {
+    background-color: #61b8a8;
+    border-bottom-color: #61b8a8;
+  }
+
+  @media (max-width: 1440px) {
+    width: 220px;
+    height: 40px;
+    font-size: 14px;
+  }
+  @media (max-width: 1024px) {
+    width: 150px;
+    height: 45px;
+    font-size: 13px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+    max-width: 80px;
+    height: 65px;
+    font-size: 12px;
+  }
+  @media (max-width: 425px) {
+    max-width: 360px;
+    height: 34px;
+    font-size: 11px;
+  }
+  @media (max-width: 390px) {
+    max-width: 360px;
+    height: 34px;
+    font-size: 11px;
+  }
+  @media (max-width: 375px) {
+    max-width: 150px; 
+    height: 32px;
+    font-size: 10px;
+  }
+`;
+
+const SecondaryButton = styled.button`
+  width: 250px;
+  height: 45px;
+  font-family: "Orbitron", sans-serif;
+  font-weight: 500;
+  font-size: 15px;
+  color: white;
+  border: 3px solid #61b8a8;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:hover {
+    border-color: #61b8a8;
+  }
+
+  @media (max-width: 1440px) {
+    width: 220px; 
+    height: 40px;
+    font-size: 14px;
+  }
+  @media (max-width: 1024px) {
+    width: 150px;
+    height: 45px;
+    font-size: 13px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+    max-width: 80px;
+    height: 65px;
+    font-size: 12px;
+  }
+  @media (max-width: 425px) {
+    max-width: 360px;
+    height: 34px;
+    font-size: 11px;
+  }
+  @media (max-width: 390px) {
+    max-width: 360px;
+    height: 34px;
+    font-size: 11px;
+  }
+  @media (max-width: 375px) {
+    max-width: 150px; 
+    height: 32px;
+    font-size: 10px;
+  }
+`;
+
+const ImageSection = styled.div`
+  position: relative;
+`;
+
+const GamerImage = styled.img`
+  width: 700px;
+  height: 650px;
+
+  @media (max-width: 1440px) {
+    width: 600px;
+    height: 550px;
+  }
+  @media (max-width: 1024px) {
+    width: 500px;
+    height: 450px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+    width: 350px;
+    max-width: 800px;
+    height: auto;
+  }
+  @media (max-width: 425px) {
+    max-width: 500px;
+  }
+  @media (max-width: 390px) {
+    max-width: 500px;
+  }
+  @media (max-width: 375px) {
+    max-width: 280px; 
+  }
+`;
+
+const ScholarshipBox = styled.div`
+  border: 2px solid #fbad04;
+  padding: 10px;
+  border-radius: 8px;
+  width: 250px;
+  background-color: #111827;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  position: absolute;
+  top: 380px;
+  left: 450px;
+
+  @media (max-width: 1440px) {
+    width: 220px;
+    top: 320px;
+    left: 330px;
+  }
+  @media (max-width: 1155px) {
+    width: 200px;
+    top: 250px;
+    left: 220px;
+  }
+  @media (max-width: 1024px) {
+    width: 200px;
+    top: 250px;
+    left: 220px;
+  }
+  @media (max-width: 945px) {
+    position: absolute; 
+    max-width: 200px;
+    padding: 8px;
+    top: 180px;
+    left: 40px;
+  }
+  @media (max-width: 769px) {
+    position: absolute; 
+    max-width: 200px;
+    padding: 8px;
+    top: 180px;
+    left: 40px;
+  }
+
+  @media (max-width: 768px) {
+    position: absolute; 
+    max-width: 300px;
+    padding: 8px;
+    top: 180px;
+    left: 70px;
+  }
+  @media (max-width: 425px) {
+    max-width: 280px;
+    padding: 6px;
+    top: 200px;
+  }
+  @media (max-width: 390px) {
+    max-width: 280px;
+    padding: 6px;
+    top: 200px;
+  }
+  @media (max-width: 375px) {
+    max-width: 260px;
+    padding: 6px;
+  }
+`;
+
+const ScholarshipText = styled.p`
+  font-family: "Orbitron", sans-serif;
+  font-weight: 500;
+  font-size: 20px;
+  color: #fbad04;
+
+  @media (max-width: 1440px) {
+    font-size: 16px;
+  }
+  @media (max-width: 1024px) {
+    font-size: 15px; 
+  }
+  @media (max-width: 769px) {
+    font-size: 12px;
+  }
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
+  @media (max-width: 390px) {
+    font-size: 12px;
+  }
+  @media (max-width: 375px) {
+    font-size: 11px; 
+  }
+`;
+
+const ScholarshipDescription = styled.p`
+  color: white;
+  font-size: 16px; 
+
+  @media (max-width: 1440px) {
+    font-size: 15px;
+  }
+  @media (max-width: 1024px) {
+    font-size: 14px; 
+  }
+  @media (max-width: 768px) {
+    font-size: 13px; 
+  }
+  @media (max-width: 390px) {
+    font-size: 12px; 
+  }
+  @media (max-width: 375px) {
+    font-size: 11px;
+  }
+`;
+
+const ScholarshipFlex = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+`;
--- src/pages/GameDev/Components/HelpCenter.jsx
+++ src/pages/GameDev/Components/HelpCenter.jsx
@@ -1,65 +1,419 @@
-import red from '../../../assets/gamedev-assets/red.svg'
-import orange from '../../../assets/gamedev-assets/orange.svg'
-import green from '../../../assets/gamedev-assets/green.svg'
-import q1 from '../../../assets/gamedev-assets/q1.svg'
-import q2 from '../../../assets/gamedev-assets/q2.svg'
-import q3 from '../../../assets/gamedev-assets/q3.svg'
-import q4 from '../../../assets/gamedev-assets/q4.svg'
-import q5 from '../../../assets/gamedev-assets/q5.svg'
-import { useState } from 'react'
+import styled from "styled-components";
+import red from "../../../assets/gamedev-assets/red.svg";
+import orange from "../../../assets/gamedev-assets/orange.svg";
+import green from "../../../assets/gamedev-assets/green.svg";
+import q1 from "../../../assets/gamedev-assets/q1.svg";
+import q2 from "../../../assets/gamedev-assets/q2.svg";
+import q3 from "../../../assets/gamedev-assets/q3.svg";
+import q4 from "../../../assets/gamedev-assets/q4.svg";
+import q5 from "../../../assets/gamedev-assets/q5.svg";
+import { useState } from "react";
+import "@fontsource/orbitron";
 
 const HelpCenter = () => {
   const faqData = [
-  { id: 1, image: q1, question: "Is this a program paid for?", answer: "Our program is fully funded, and selected participants will receive full scholarships covering all training expenses. This is a golden opportunity to advance your career in tech without financial barriers!" },
-  { id: 2, image: q2, question: "Who can Apply for our Programs?", answer: "Our programs are designed to be beginner-friendly, well-structured, and comprehensive. Whether you're starting fresh or looking to upskill, we’ve got something for everyone. Upon successful completion, you’ll also receive a certificate to showcase your new skills!" },
-  { id: 3, image: q3, question: "What is the learning duration?", answer: "Our program duration ranges between 3 to 6 months, depending on the course. All programs are thoughtfully structured to ensure you gain deep, hands-on experience while balancing flexibility." },
-  { id: 4, image: q4, question: "How Will the Training Be Delivered?", answer: "The delivery method depends on the program you apply for. Our programs are designed to offer flexibility and are delivered through in-person, online, or hybrid approaches, ensuring you receive the best learning experience based on the program's format." },
-  { id: 5, image: q5, question: "What Are the Prerequisites?", answer: "No prior experience in tech is required! All you need is a university degree (OND, HND, or BSc) and a strong desire to learn and grow. We're here to help you develop the skills you need for success!" },
-];
+    {
+      id: 1,
+      image: q1,
+      question: "Is this a program paid for?",
+      answer:
+        "Our program is fully funded, and selected participants will receive full scholarships covering all training expenses. This is a golden opportunity to advance your career in tech without financial barriers!",
+    },
+    {
+      id: 2,
+      image: q2,
+      question: "Who can Apply for our Programs?",
+      answer:
+        "Our programs are designed to be beginner-friendly, well-structured, and comprehensive. Whether you're starting fresh or looking to upskill, we’ve got something for everyone. Upon successful completion, you’ll also receive a certificate to showcase your new skills!",
+    },
+    {
+      id: 3,
+      image: q3,
+      question: "What is the learning duration?",
+      answer:
+        "Our program duration ranges between 3 to 6 months, depending on the course. All programs are thoughtfully structured to ensure you gain deep, hands-on experience while balancing flexibility.",
+    },
+    {
+      id: 4,
+      image: q4,
+      question: "How Will the Training Be Delivered?",
+      answer:
+        "The delivery method depends on the program you apply for. Our programs are designed to offer flexibility and are delivered through in-person, online, or hybrid approaches, ensuring you receive the best learning experience based on the program's format.",
+    },
+    {
+      id: 5,
+      image: q5,
+      question: "What Are the Prerequisites?",
+      answer:
+        "No prior experience in tech is required! All you need is a university degree (OND, HND, or BSc) and a strong desire to learn and grow. We're here to help you develop the skills you need for success!",
+    },
+  ];
 
-const [openFAQ, setOpenFAQ] = useState(null);
+  const [openFAQ, setOpenFAQ] = useState(null);
 
   const toggleFAQ = (id) => {
     setOpenFAQ(openFAQ === id ? null : id);
   };
 
-    return (
-        <div className="flex justify-center items-center w-full p-[60px] bg-[#111827]">
-            <div className="flex flex-col items-center justify-center text-center gap-4">
-                <p className="font-[orbitron] text-[#FBAD04] text-[12px]">GAME MANUAL</p>
-                <h1 className="font-[orbitron] font-bold text-white text-[25px]">HELP CENTER</h1>
-                <section className='bg-[#FBAD04] w-[100px] h-[5px]'></section>
-                <p className="text-gray-400">Get answers to the most common questions about our game development programme.</p>
-
-                <div className="bg-[#1F2937] rounded-lg shadow-md w-full max-w-3xl">
-                    <div className='flex items-center p-2 gap-[160px]'>
-                        <div className='flex items-center gap-3'>
-                            <img src={red} alt="red circle" />
-                            <img src={orange} alt="orange circle" />
-                            <img src={green} alt="green circle" />
-                        </div>
-                        <p className="text-gray-400 font-[orbitron] text-[15px]">FAQ DATABASE.exe</p>
-                    </div>
-                    <div className='border-1 border-gray-400 rounded-lg'>
-                        {faqData.map((faq) => (
-                        <div key={faq.id} className="w-full p-2 cursor-pointer" onClick={() => toggleFAQ(faq.id)}>
-                            <div className="flex items-center gap-4 rounded-lg bg-[#111827] p-3">
-                                <img src={faq.image} alt="Icon" className="w-[20px] h-[20px]" />
-                                <h2 className="text-[#FBAD04] font-[orbitron] text-[12px]">{faq.question}</h2>
-                            </div>
-
-                            {openFAQ === faq.id && (
-                            <p className="text-gray-300 font-[orbitron] text-[10px] mt-2">{faq.answer}</p>
-                            )}
-                        </div>
-                      ))}
-
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    )
-}
+  return (
+    <HelpCenterContainer>
+      <InnerContainer>
+        <Subheading>GAME MANUAL</Subheading>
+        <Heading>HELP CENTER</Heading>
+        <YellowBar />
+        <Description>
+          Get answers to the most common questions about our game development programme.
+        </Description>
+        <FAQContainer>
+          <FAQHeader>
+            <IconContainer>
+              <img src={red} alt="red circle" />
+              <img src={orange} alt="orange circle" />
+              <img src={green} alt="green circle" />
+            </IconContainer>
+            <FAQHeaderText>FAQ DATABASE.exe</FAQHeaderText>
+          </FAQHeader>
+          <FAQList>
+            {faqData.map((faq) => (
+              <FAQItem key={faq.id} onClick={() => toggleFAQ(faq.id)}>
+                <FAQQuestion>
+                  <FAQIcon src={faq.image} alt="Icon" />
+                  <FAQQuestionText>{faq.question}</FAQQuestionText>
+                </FAQQuestion>
+                {openFAQ === faq.id && <FAQAnswer>{faq.answer}</FAQAnswer>}
+              </FAQItem>
+            ))}
+          </FAQList>
+        </FAQContainer>
+      </InnerContainer>
+    </HelpCenterContainer>
+  );
+};
 
 export default HelpCenter;
+
+const HelpCenterContainer = styled.div`
+  width: 100%;
+  padding: 60px; 
+  background-color: #111827;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 1440px) {
+    padding: 48px; 
+  }
+  @media (max-width: 1024px) {
+    padding: 40px; 
+  }
+  @media (max-width: 768px) {
+    padding: 32px;
+  }
+  @media (max-width: 390px) {
+    padding: 24px; 
+  }
+  @media (max-width: 375px) {
+    padding: 16px;
+  }
+`;
+
+const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  gap: 16px; 
+
+  @media (max-width: 768px) {
+    gap: 12px; 
+  }
+  @media (max-width: 390px) {
+    gap: 10px; 
+  }
+  @media (max-width: 375px) {
+    gap: 8px; 
+  }
+`;
+
+const Subheading = styled.p`
+  font-family: "Orbitron", sans-serif;
+  color: #fbad04;
+  font-size: 12px;
+
+  @media (max-width: 1440px) {
+    font-size: 11px;
+  }
+  @media (max-width: 1024px) {
+    font-size: 10px; 
+  }
+  @media (max-width: 768px) {
+    font-size: 9px; 
+  }
+  @media (max-width: 390px) {
+    font-size: 8px; 
+  }
+  @media (max-width: 375px) {
+    font-size: 8px;
+  }
+`;
+
+const Heading = styled.h1`
+  font-family: "Orbitron", sans-serif;
+  font-weight: 700;
+  color: white;
+  font-size: 25px;
+
+  @media (max-width: 1440px) {
+    font-size: 22px; 
+  }
+  @media (max-width: 1024px) {
+    font-size: 20px; 
+  }
+  @media (max-width: 768px) {
+    font-size: 18px;
+  }
+  @media (max-width: 390px) {
+    font-size: 16px;
+  }
+  @media (max-width: 375px) {
+    font-size: 14px; 
+  }
+`;
+
+const YellowBar = styled.section`
+  background-color: #fbad04;
+  width: 100px;
+  height: 5px;
+
+  @media (max-width: 1440px) {
+    width: 80px; 
+  }
+  @media (max-width: 1024px) {
+    width: 70px; 
+  }
+  @media (max-width: 768px) {
+    width: 60px; 
+  }
+  @media (max-width: 390px) {
+    width: 50px; 
+  }
+  @media (max-width: 375px) {
+    width: 40px;
+  }
+`;
+
+const Description = styled.p`
+  color: #9ca3af; 
+  font-size: 16px;
+
+  @media (max-width: 1440px) {
+    font-size: 15px; 
+  }
+  @media (max-width: 1024px) {
+    font-size: 14px; 
+  }
+  @media (max-width: 768px) {
+    font-size: 13px; 
+  }
+  @media (max-width: 390px) {
+    font-size: 12px; 
+  }
+  @media (max-width: 375px) {
+    font-size: 11px;
+  }
+`;
+
+const FAQContainer = styled.div`
+  background-color: #1f2937;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); 
+  width: 100%;
+  max-width: 768px;
+
+  @media (max-width: 1440px) {
+    max-width: 700px; 
+  }
+  @media (max-width: 1024px) {
+    max-width: 600px; 
+  }
+  @media (max-width: 768px) {
+    max-width: 90%; 
+  }
+  @media (max-width: 390px) {
+    max-width: 100%; 
+  }
+  @media (max-width: 375px) {
+    max-width: 100%; 
+  }
+`;
+
+const FAQHeader = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 8px; 
+  gap: 160px;
+
+  @media (max-width: 1440px) {
+    gap: 120px; 
+  }
+  @media (max-width: 1024px) {
+    gap: 80px; 
+  }
+  @media (max-width: 768px) {
+    gap: 40px; 
+  }
+  @media (max-width: 390px) {
+    gap: 20px; 
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  @media (max-width: 375px) {
+    gap: 16px; 
+  }
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px; 
+
+  @media (max-width: 768px) {
+    gap: 8px; 
+  }
+  @media (max-width: 390px) {
+    gap: 6px;
+  }
+  @media (max-width: 375px) {
+    gap: 4px; 
+  }
+`;
+
+const FAQHeaderText = styled.p`
+  color: #9ca3af; 
+  font-family: "Orbitron", sans-serif;
+  font-size: 15px;
+
+  @media (max-width: 1440px) {
+    font-size: 14px; 
+  }
+  @media (max-width: 1024px) {
+    font-size: 13px; 
+  }
+  @media (max-width: 768px) {
+    font-size: 12px;
+  }
+  @media (max-width: 390px) {
+    font-size: 11px; 
+  }
+  @media (max-width: 375px) {
+    font-size: 10px;
+  }
+`;
+
+const FAQList = styled.div`
+  border: 1px solid #9ca3af; 
+  border-radius: 8px;
+`;
+
+const FAQItem = styled.div`
+  width: 100%;
+  padding: 8px;
+  cursor: pointer;
+
+  @media (max-width: 768px) {
+    padding: 6px; 
+  }
+  @media (max-width: 390px) {
+    padding: 4px;
+  }
+  @media (max-width: 375px) {
+    padding: 4px;
+  }
+`;
+
+const FAQQuestion = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px; 
+  border-radius: 8px;
+  background-color: #111827;
+  padding: 12px; 
+
+  @media (max-width: 768px) {
+    gap: 12px;
+    padding: 10px;
+  }
+  @media (max-width: 390px) {
+    gap: 8px;
+    padding: 8px;
+  }
+  @media (max-width: 375px) {
+    gap: 6px;
+    padding: 6px;
+  }
+`;
+
+const FAQIcon = styled.img`
+  width: 20px;
+  height: 20px;
+
+  @media (max-width: 768px) {
+    width: 18px;
+    height: 18px;
+  }
+  @media (max-width: 390px) {
+    width: 16px; 
+    height: 16px;
+  }
+  @media (max-width: 375px) {
+    width: 14px;
+    height: 14px;
+  }
+`;
+
+const FAQQuestionText = styled.h2`
+  color: #fbad04;
+  font-family: "Orbitron", sans-serif;
+  font-size: 12px;
+
+  @media (max-width: 1440px) {
+    font-size: 11px; 
+  }
+  @media (max-width: 1024px) {
+    font-size: 10px; 
+  }
+  @media (max-width: 768px) {
+    font-size: 9px;
+  }
+  @media (max-width: 390px) {
+    font-size: 8px; 
+  }
+  @media (max-width: 375px) {
+    font-size: 8px;
+  }
+`;
+
+const FAQAnswer = styled.p`
+  color: #d1d5db; 
+  font-family: "Orbitron", sans-serif;
+  font-size: 10px;
+  margin-top: 8px;
+  text-align: left;
+
+  @media (max-width: 1440px) {
+    font-size: 9px; 
+  }
+  @media (max-width: 1024px) {
+    font-size: 9px; 
+  }
+  @media (max-width: 768px) {
+    font-size: 8px; 
+  }
+  @media (max-width: 390px) {
+    font-size: 7px; 
+  }
+  @media (max-width: 375px) {
+    font-size: 7px; 
+  }
+`;
